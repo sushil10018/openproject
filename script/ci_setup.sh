fc42dbce995825ad875af5dff176f9dd1978543b
Jens Ulferts <j.ulferts@finn.de>
2017-04-28 00:10:24 +0200

    wip

--- script/ci_setup.sh
+++ script/ci_setup.sh
@@ -64,7 +64,7 @@ fi
 # run migrations for mysql or postgres
 if [ $1 != 'npm' ]; then
   run "bundle exec rake db:migrate"
-  run "bundle install --binstub"
+  run "bundle install --binstubs"
 fi
 
 if [ $1 != 'specs' ] && [ $1 != 'spec_legacy' ]; then
